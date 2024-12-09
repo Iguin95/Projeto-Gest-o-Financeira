841fe11fdcfe9d4cea722398870478b1b723b878
Igor Vinnícius <igorquintao22@gmail.com>
2024-12-09 16:11:01 -0300

    Status 500 resolvido. Foi usado JsonIgnore

--- src/main/java/com/financas/GestaoFinanceira/domain/Category.java
+++ src/main/java/com/financas/GestaoFinanceira/domain/Category.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -59,6 +61,7 @@ public class Category implements Serializable{
 		this.predictedCategoryLimit = predictedCategoryLimit;
 	}
 
+	@JsonIgnore
 	public Set<CategoryExpense> getExpenses() {
 		return expenses;
 	}
